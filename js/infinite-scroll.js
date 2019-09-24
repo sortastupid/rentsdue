67cb070db4bc51eee16b50e0e520b18b968ac251
Alice Moraes <amoraesx@gmail.com>
2019-09-24 06:19:17 +0100

    Initial commit

---
+++ js/infinite-scroll.js
@@ -0,0 +1,84 @@
+let _n = 2;
+let _end = false;
+let _before = 0;
+let _posts = {};
+let _mobile;
+const _url = document.URL;
+const _exec = _url.match('(/search/|/tagged/)|[/|/#]$');
+
+if (!_exec) _end = true;
+
+const showNext = () => {
+    if (!_end) {
+        const now = new Date();
+        const delta = now - _before;
+        if (delta > 1000) {
+            let posts = document.createDocumentFragment();
+            for (i = 0; i < _posts.length; i++) {
+                posts.appendChild(_posts[i]);
+            }
+            document.getElementById("posts").appendChild(posts);
+            getNext();
+        } else if (_end) {
+            $('#load-more').css("display", "none");
+            $('#the-end').css("display", "flex");
+        }
+        before = now;
+    }
+}
+
+const getNext = () => {
+    let url = _url.match('/$') && _url.slice(0, -1) || _url;
+    url = url.concat("/page/").concat(_n);
+    getAsync(url, (xml) => {
+        let html = document.createElement('div');
+        html.innerHTML = xml.trim();
+        const posts = html.querySelectorAll(".post")
+        if (posts.length > 0) {
+            _n++;
+            _posts = posts;
+            $('#load-more').css("display", "flex");
+        } else {
+            $('#load-more').css("display", "none");
+            $('#the-end').css("display", "flex");
+            _end = true;
+        }
+    });
+}
+
+const getAsync = (url, callback) => {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+!_end && getNext();
+
+$(function() {
+    const checkDevice = () => {
+        const scroll = $(document).height() - $(window).height() > 0;
+        let windowWidth = $(window).width();
+        if (scroll) windowWidth = windowWidth + 17;
+        if (windowWidth >= 675) {
+            _mobile = false;
+            $('#load-more').css("display", "none");
+        } else {
+            _mobile = true;
+            if (_exec && !_end) $('#load-more').css("display", "flex");
+        }
+    }
+    checkDevice();
+    $(window).scroll(() => {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() && !_mobile) {
+            showNext();
+        }
+    });
+    $(window).resize(() => {
+        checkDevice();
+    });
+});
---
+++ js/rentsdue.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    const vw = $(window).width();
+
+    const setTitle = () => {
+        let t_width = $("#title").width();
+        const title = document.querySelector('#title');
+        const text = title.textContent;
+        const length = text.length;
+        const width = window.innerWidth;
+        const ratio = width / t_width;
+        let cssfontsize = $("#title").css("font-size");
+        cssfontsize = parseInt(cssfontsize.slice(0, -2));
+        cssfontsize = cssfontsize * ratio;
+        let size = width / length;
+        const percent = 1.3;
+        size = size * percent;
+        size = parseInt(size);
+        size = cssfontsize * 0.95;
+
+        let value = "" + size + "px";
+        $('#title').css("font-size", value);
+        t_width = $("#title").width();
+    }
+
+    const setSpins = () => {
+        if ($(".spin-block")[0].childElementCount / 2 >= 4) {
+            $(".spin-block").clone().appendTo(".spin-up");
+            $(".spin-wrap").css("height", "" + $(".spin-up > .spin-block").height() + "px");
+        } else {
+            $(".spin-up").css("animation-play-state", "paused");
+        }
+
+        let title = $("h1").text();
+        $(".spin-left p").text(title.concat(" !!! "));
+        $(".spin-left p").css("width", "fit-content");
+        const pw = $(".spin-left p").width();
+        const factor = pw > 0 ? vw / pw : 1;
+        const n = Math.round(factor) - 1;
+        let string = "";
+        for (i = 0; i < n; i++) {
+            string = string.concat(title.concat(" !!! "));
+        }
+        $(".spin p").text(string);
+        $('.spin p').css("text-align-last", "justify");
+        $('.spin p').css("width", "auto");
+
+        const spins = [$(".spin-left"), $(".spin-right")];
+        spins.forEach((spin) => {
+            if (spin.length === 1) {
+                const parent = spin[0].parentElement;
+                spin.clone().addClass("spin-2").appendTo($(parent));
+            }
+        });
+        $(".spin").css("animation-play-state", "running");
+    }
+
+    const setLinks = () => {
+        $(".draggable").draggable();
+        $(".row-sm").css("height", $("#hidden-gif > img")[0].height);
+    }
+
+    setTitle();
+    setSpins();
+    setLinks();
+
+    $(window).resize(() => {
+        setTitle();
+        setSpins();
+        setLinks();
+    });
+});
+
+const sharePost = id => {
+    $("#".concat(id)).find("#popup").toggleClass("show");
+}
+
+const toggleModal = mid => {
+    $("#".concat(mid)).toggleClass("show-modal");
+}
+
+window.onclick = event => {
+    const modal = $('.show-modal');
+    if (modal.length > 0 && event.target.id == modal[0].id) {
+        toggleModal(modal[0].id);
+    }
+}
